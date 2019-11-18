ec71244ccd3c0cc4e8b089194ee9003006333432
Benjamin Lannon <benjamin@lannonbr.com>
2019-11-18 11:04:50 -0500

    Fixing bugs

--- index.js
+++ index.js
@@ -10,12 +10,15 @@ require("dotenv").config();
 const twitchClientID = process.env.CLIENT_ID;
 const team = process.env.TEAM_NAME;
 
-const timestamp = moment()
-  .unix()
-  .toString();
-
 exports.handler = async function() {
   return new Promise(async (resolve, reject) => {
+    let rootResolve = resolve
+    let rootReject = reject
+
+    const timestamp = moment()
+      .unix()
+      .toString();
+
     try {
       let teamURL = `https://api.twitch.tv/kraken/teams/${team}`;
       let resp = await fetch(teamURL, {
@@ -39,47 +42,38 @@ exports.handler = async function() {
       data = await resp.json();
       let onlineUsers = data.data.map(user => user.user_name);
 
+      console.log({ onlineUsers })
+
       // Terminate early if no one is online
       if (onlineUsers.length === 0) {
-        resolve(200);
+        rootResolve(200);
       }
 
       const params = {
         RequestItems: {
-          LiveCodersStreamPoints: []
+          LiveCodersStreamPoints: onlineUsers.map(user => ({
+            PutRequest: {
+              Item: {
+                username: { S: user },
+                timestamp: { N: timestamp }
+              }
+            }
+          }))
         }
       };
 
-      onlineUsers.forEach(user => {
-        params.RequestItems.LiveCodersStreamPoints.push({
-          PutRequest: {
-            Item: {
-              username: { S: user },
-              timestamp: { N: timestamp }
-            }
-          }
-        });
-      });
+      let response = await ddb.batchWriteItem(params).promise()
 
-      ddb.batchWriteItem(params, processCallback);
+      while(response.UnprocessedItems.LiveCodersStreamPoints && response.UnprocessedItems.LiveCodersStreamPoints.length > 0) {
+        params.RequestItems = response.UnprocessedItems
+        response = await ddb.batchWriteItem(params).promise()
+      }
+
+      rootResolve(200)
+
     } catch (err) {
       console.error(err);
-      reject(err);
-    }
-
-    function processCallback(err, data) {
-      if (err) {
-        console.error(err);
-        reject(err);
-      } else {
-        if (data.UnprocessedItems.LiveCodersStreamPoints && data.UnprocessedItems.LiveCodersStreamPoints.length > 0) {
-          let params = {}
-          params.RequestItems = data.UnprocessedItems
-          ddb.batchWriteItem(params, processCallback);
-        } else {
-          resolve(200)
-        }
-      }
+      rootReject(err);
     }
   });
 };
