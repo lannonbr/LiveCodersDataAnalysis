8bdfc9b021e65f1f6c1e24bf3cf91bc0d0d3b614
Benjamin Lannon <benjamin@lannonbr.com>
2019-11-18 20:25:31 -0500

    Wrap with prettier

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const fetch = require("node-fetch");
-const moment = require("moment");
+const fetch = require("node-fetch")
+const moment = require("moment")
 
-const AWS = require("aws-sdk");
-AWS.config.update({ region: "us-east-1" });
-const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+const AWS = require("aws-sdk")
+AWS.config.update({ region: "us-east-1" })
+const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" })
 
-require("dotenv").config();
+require("dotenv").config()
 
-const twitchClientID = process.env.CLIENT_ID;
-const team = process.env.TEAM_NAME;
+const twitchClientID = process.env.CLIENT_ID
+const team = process.env.TEAM_NAME
 
 exports.handler = async function() {
   return new Promise(async (resolve, reject) => {
@@ -17,36 +17,36 @@ exports.handler = async function() {
 
     const timestamp = moment()
       .unix()
-      .toString();
+      .toString()
 
     try {
-      let teamURL = `https://api.twitch.tv/kraken/teams/${team}`;
+      let teamURL = `https://api.twitch.tv/kraken/teams/${team}`
       let resp = await fetch(teamURL, {
         headers: {
           Accept: "application/vnd.twitchtv.v5+json",
-          "Client-ID": twitchClientID
-        }
-      });
-      let data = await resp.json();
-      let members = data.users;
+          "Client-ID": twitchClientID,
+        },
+      })
+      let data = await resp.json()
+      let members = data.users
 
-      let uids = members.map(member => member._id);
-      let idStr = uids.join("&user_id=");
+      let uids = members.map(member => member._id)
+      let idStr = uids.join("&user_id=")
 
-      let activeStreamsURL = `https://api.twitch.tv/helix/streams?user_id=${idStr}`;
+      let activeStreamsURL = `https://api.twitch.tv/helix/streams?user_id=${idStr}`
       resp = await fetch(activeStreamsURL, {
         headers: {
-          "Client-ID": twitchClientID
-        }
-      });
-      data = await resp.json();
-      let onlineUsers = data.data.map(user => user.user_name);
+          "Client-ID": twitchClientID,
+        },
+      })
+      data = await resp.json()
+      let onlineUsers = data.data.map(user => user.user_name)
 
       console.log({ onlineUsers })
 
       // Terminate early if no one is online
       if (onlineUsers.length === 0) {
-        rootResolve(200);
+        rootResolve(200)
       }
 
       const params = {
@@ -55,25 +55,27 @@ exports.handler = async function() {
             PutRequest: {
               Item: {
                 username: { S: user },
-                timestamp: { N: timestamp }
-              }
-            }
-          }))
-        }
-      };
+                timestamp: { N: timestamp },
+              },
+            },
+          })),
+        },
+      }
 
       let response = await ddb.batchWriteItem(params).promise()
 
-      while(response.UnprocessedItems.LiveCodersStreamPoints && response.UnprocessedItems.LiveCodersStreamPoints.length > 0) {
+      while (
+        response.UnprocessedItems.LiveCodersStreamPoints &&
+        response.UnprocessedItems.LiveCodersStreamPoints.length > 0
+      ) {
         params.RequestItems = response.UnprocessedItems
         response = await ddb.batchWriteItem(params).promise()
       }
 
       rootResolve(200)
-
     } catch (err) {
-      console.error(err);
-      rootReject(err);
+      console.error(err)
+      rootReject(err)
     }
-  });
-};
+  })
+}
